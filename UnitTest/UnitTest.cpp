2877e16d020a792cd3ddcb8ffe69ed0d3db14cd6
Mehak Kondal <Mkondal4243@conestogac.on.ca>
2023-11-18 20:40:26 -0500

    #Added more Triangle test case Functionalities #Arranged test cases in proper order with comments

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -13,7 +13,8 @@ namespace UnitTest
 	{
 	public:
 		
-		TEST_METHOD(TestEquilateralTriangleFunctionality)
+		//Tests for Equilateral Triangle
+		TEST_METHOD(TestEquilateralTriangleFunctionality)  
 		{
 			int sides[3] = { 5, 5, 5 };
 
@@ -22,6 +23,21 @@ namespace UnitTest
 			Assert::AreEqual(result, "Equilateral triangle");
 		}
 
+		TEST_METHOD(TestEquilateralTriangleDecimalFunctionality)  
+		{
+			int sides[3] = { 3.5, 3.5, 3.5 };
+			char* result = analyzeTriangle(sides[0], sides[1], sides[2]);
+			Assert::AreEqual(result, "Equilateral triangle");
+		}
+
+		TEST_METHOD(TestEquilateralTriangleLargerValFunctionality)
+		{
+			int sides[3] = { 10, 10, 10 };
+			char* result = analyzeTriangle(sides[0], sides[1], sides[2]);
+			Assert::AreEqual(result, "Equilateral triangle");
+		}
+
+		//tests for isoceles triangle 
 		TEST_METHOD(TestIsoscelesTriangleFunctionality)
 		{
 			
@@ -32,6 +48,22 @@ namespace UnitTest
 			Assert::AreEqual(result, "Isosceles triangle");
 		}
 
+		TEST_METHOD(TestIsoscelesTriangleDecimalFunctionality)
+		{
+			int sides[3] = { 5.5, 5.5, 7 };
+			char* result = analyzeTriangle(sides[0], sides[1], sides[2]);
+			Assert::AreEqual(result, "Isosceles triangle");
+		}
+
+		TEST_METHOD(TestIsoscelesTriangleLargerValFunctionality)
+		{
+			int sides[3] = { 8, 8, 10 };
+			char* result = analyzeTriangle(sides[0], sides[1], sides[2]);
+			Assert::AreEqual(result, "Isosceles triangle");
+		}
+
+
+		//tests for scalene triangle 
 		TEST_METHOD(TestScaleneTriangleFunctionality)
 		{
 			int sides[3] = { 3, 4, 5 };
@@ -41,7 +73,22 @@ namespace UnitTest
 			Assert::AreEqual(result, "Scalene triangle");
 		}
 
-		TEST_METHOD(TestInvalidTriangleFunctionality)
+		TEST_METHOD(TestScaleneTriangleDecimalValuesFunctionality)
+		{
+			int sides[3] = { 2.5, 3.5, 4.5 };
+			char* result = analyzeTriangle(sides[0], sides[1], sides[2]);
+			Assert::AreEqual(result, "Scalene triangle");
+		}
+
+		TEST_METHOD(TestScaleneTriangleLargerValFunctionality)
+		{
+			int sides[3] = { 7, 10, 12 };
+			char* result = analyzeTriangle(sides[0], sides[1], sides[2]);
+			Assert::AreEqual(result, "Scalene triangle");
+		}
+
+		//invalid test cases 
+		TEST_METHOD(TestInvalidTriangleFunctionality)  
 		{
 			int sides[3] = { 1, 1, 2 };
 
@@ -50,6 +97,22 @@ namespace UnitTest
 			Assert::AreEqual(result, "Not a triangle");
 		}
 
+
+		TEST_METHOD(TestInvalidNegativeSideLengths) 
+		{
+			int sides[3] = { -3, 4, 5 };
+			char* result = analyzeTriangle(sides[0], sides[1], sides[2]);
+			Assert::AreEqual(result, "Not a triangle");
+		}
+
+		TEST_METHOD(TestInvalidZeroSideLengths) 
+		{
+			int sides[3] = { 0, 4, 5 };
+			char* result = analyzeTriangle(sides[0], sides[1], sides[2]);
+			Assert::AreEqual(result, "Not a triangle");
+		}
+
+		//angles test cases 
 		TEST_METHOD(TestCalculateTriangleAnglesEquilateral)
 		{
 			
@@ -71,7 +134,7 @@ namespace UnitTest
 
 			calculateTriangleAngles(sides, angles);
 
-			Assert::AreEqual(angles[0], 44.0, 0.01); // Adjust the epsilon as needed
+			Assert::AreEqual(angles[0], 44.0, 0.01); 
 			Assert::AreEqual(angles[1], 44.0, 0.01);
 			Assert::AreEqual(angles[2], 91.0, 0.01);
 			
@@ -88,5 +151,14 @@ namespace UnitTest
 			Assert::AreEqual(angles[1], 53.0, 0.01);
 			Assert::AreEqual(angles[2], 90.0, 0.01);
 		}
+
+		TEST_METHOD(TestSumOfAngles)  
+		{
+			int sides[3] = { 3, 4, 5 };
+			double angles[3] = { 0, 0, 0 };
+			calculateTriangleAngles(sides, angles);
+			Assert::AreEqual(angles[0] + angles[1] + angles[2], 180.0, 0.01);
+		}
+
 	};
 }
