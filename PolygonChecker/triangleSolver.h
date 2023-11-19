d8868fe017c9d3f1dbe7f402c4d6f63063d3011a
Mehak Kondal <Mkondal4243@conestogac.on.ca>
2023-11-18 22:00:50 -0500

    #

--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,9 +1,7 @@
-//triangleSolver.h
-
 #pragma once
 
 
-char* analyzeTriangle(int side1, int side2, int side3);
+char* analyzeTriangle(int side1, int side2, int side3); 
 char* classifyTriangle(const int* sides);  
 void calculateTriangleAngles(const int* sides, double* angles);
 
